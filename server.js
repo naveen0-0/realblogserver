88c44a301469b94407b102cfef3b15449289591a
Naveen <naveen.k@mtechzilla.com>
2023-02-23 08:44:30 +0530

    fix: changed mongo url

--- server.js
+++ server.js
@@ -1,41 +1,41 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const dotenv = require('dotenv')
-const authRoutes = require('./routes/authen');
-const apiRoutes = require('./routes/api')
+const express = require("express");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authen");
+const apiRoutes = require("./routes/api");
 //* App Initialization
 const app = express();
 
 //*Middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(helmet())
-app.use(morgan('tiny'))
-app.use(cookieParser())
-app.use(cors({ origin: "https://blogreal.netlify.app" }));
-dotenv.config()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("tiny"));
+app.use(cookieParser());
+app.use(cors({ origin: "*" }));
+dotenv.config();
 
-app.get('/',(req,res)=>{
-        res.send("BlogReal Application")
-})
+app.get("/", (req, res) => {
+  res.send("BlogReal Application");
+});
 
 //*Routes
-app.use('/auth',authRoutes)
-app.use('/api',apiRoutes)
+app.use("/auth", authRoutes);
+app.use("/api", apiRoutes);
 
 //* MongoDB Connection
 const MONGO = process.env.MONGO || "mongodb://localhost/blogreal";
 
-const options = { useNewUrlParser:true, useUnifiedTopology:true }
-mongoose.connect(MONGO,options)
-        .then(() => console.log("Mongo connection successful"))
-        .catch(() => console.log("Mongo connection failure"))
-
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose
+  .connect(MONGO, options)
+  .then(() => console.log("Mongo connection successful"))
+  .catch(() => console.log("Mongo connection failure"));
 
 //* Listening Port
-const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
